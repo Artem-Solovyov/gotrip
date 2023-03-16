caa270fdc4a9413f2172cb158ddb186bbe5e7d8e
HP-UltraBook <solovevarem892@gmail.com>
2023-03-16 11:41:14 +0200

    mobile adapt

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-var owl = $(".owl-carousel");
+// Carousel
+
+const owl = $(".owl-carousel");
 owl.owlCarousel({
   center: true,
   loop: true,
-  margin: 30,
+  margin: 15,
   startPosition: 1,
+  responsive: {
+    480: {
+      margin: 15,
+    },
+    1000: {
+      margin: 20,
+    },
+    1200: {
+      margin: 30,
+    },
+  },
 });
 $(".slider__button--prev").click(function () {
   owl.trigger("prev.owl.carousel", [300]);
@@ -11,3 +24,14 @@ $(".slider__button--prev").click(function () {
 $(".slider__button--next").click(function () {
   owl.trigger("next.owl.carousel");
 });
+
+// Menu-icon
+const navBtn = document.querySelector(".nav__togle");
+const menuIcon = document.querySelector(".menu-icon");
+const menu = document.querySelector(".nav");
+
+navBtn.onclick = function () {
+  menuIcon.classList.toggle("menu-icon-active");
+  menu.classList.toggle("nav--mobile");
+  document.body.classList.toggle("no-scrolle");
+};
